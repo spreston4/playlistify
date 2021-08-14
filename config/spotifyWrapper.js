956a53ffe7ffa7ba3991e995c4b954a032eb5707
RSimer <65471088+RSimer@users.noreply.github.com>
2021-08-14 10:38:03 -0700

    putting in login information which can lead to supplying an access token

--- config/spotifyWrapper.js
+++ config/spotifyWrapper.js
@@ -1,6 +1,7 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: ' f61cd24f13634874a2c1dfc3411dd2a5',
-    clientSecret: '55022fe59a554d10a4640dbb472ad89e'
+    clientId: 'f61cd24f13634874a2c1dfc3411dd2a5',
+    clientSecret: '55022fe59a554d10a4640dbb472ad89e',
+    redirectUri: ' http://localhost/api/users/login'
   });
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
+const spotifyWrapper = require('../config/spotifyWrapper');
 
 router.get('/', async (req, res) => {
   try {
@@ -76,7 +77,22 @@ router.get('/login', (req, res) => {
     return;
   }
 
-  res.render('login');
+  var scopes = ['user-read-private', 'user-read-email'],
+  redirectUri = 'http://localhost/api/users/login',
+  clientId = 'f61cd24f13634874a2c1dfc3411dd2a5',
+  state = 'some-state-of-my-choice';
+
+// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+var spotifyApi = new SpotifyWebApi({
+  redirectUri: redirectUri,
+  clientId: clientId
+});
+
+// Create the authorization URL
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+// https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
+console.log(authorizeURL);
 });
 
 module.exports = router;
