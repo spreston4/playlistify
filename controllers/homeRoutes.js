f9431d51f1afab39f4b7c57f6afdc0686c50993b
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-08-15 20:37:44 -0700

    This is an intial HTML build. Main has been fleshed out, wiht button interactivity so you dont have to keep typing the values into the URL bar. We still need to adjust data.body output on search. and we also need to work on rendering the db values into the html

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -67,38 +67,7 @@ router.get('/project/:id', async (req, res) => {
 */
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
-  const code = req.query.code;
-  const spotifyApi = spotifyApiFactory();
-  try {
-    spotifyApi.authorizationCodeGrant(code).then(
-      function(data) {
-        console.log('The token expires in ' + data.body['expires_in']);
-        console.log('The access token is ' + data.body['access_token']);
-        console.log('The refresh token is ' + data.body['refresh_token']);
-    
-        // Set the access token on the API object to use it in later calls
-        spotifyApi.setAccessToken(data.body['access_token']);
-        spotifyApi.setRefreshToken(data.body['refresh_token']);
-        console.log(data.body);
-        req.session.save(() => {
-          req.session.spotifyApi = spotifyApi;
-          req.session.logged_in = true;
-
-
-          
-          res.redirect('http://localhost:3001/');
-
-        });
-      },
-      function(err) {
-        console.log('Something went wrong!', err);
-      }
-    );
-
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  res.render('profile');
 });
 
 router.get('/login', (req, res) => {
---
+++ public/js/search.js
@@ -0,0 +1,14 @@
+const searching = async () => {
+    const response = await fetch('/api/spotify/search/tracks', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#Searching').addEventListener('click', searching);
