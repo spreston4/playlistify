e49040e41dae3d8a11f4b6186d4822afb4c7ea1a
RSimer <65471088+RSimer@users.noreply.github.com>
2021-08-16 20:58:00 -0700

    comments for future reference

--- controllers/api/spotifyRoutes.js
+++ controllers/api/spotifyRoutes.js
@@ -4,10 +4,12 @@ const spotifyApiFactory = require('../../config/spotifyWrapper');
 router.get('/search/tracks', async (req,res) => { 
 console.log(req.session.spotifyApi);
 const spotifyApi = spotifyApiFactory(req.session.access_token, req.session.refresh_token);  
-spotifyApi.searchTracks('Love')
+spotifyApi.searchTracks(req.body.search)
   .then(function(data) {
+   
+      console.log('Search by "Love"', data.body.tracks.items);
+    // put an {{each}} into the search template and a res.render so that we can see each individual item
     
-    console.log('Search by "Love"', data.body.tracks.items);
   }, function(err) {
     console.error(err);
   });
