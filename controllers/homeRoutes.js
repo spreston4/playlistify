25a76e9450bd7c6d6058c90f1f0c2961acab2be5
spreston <sam.preston11@gmail.com>
2021-08-18 18:11:44 -0500

    main, djbooth, homepage handlebars created.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,4 +79,18 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+// Render route for searchtracks page
+router.get('/searchtracks', withAuth, (req, res) => {
+
+  try {
+    res.render('searchtracks', {
+      logged_in: req.session.logged_in,
+      user_id: req.session.user_id
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
