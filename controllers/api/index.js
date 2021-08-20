a3c297fe07d99274c687a33fffe06e220c4d3dd2
spreston <sam.preston11@gmail.com>
2021-08-20 17:24:30 -0500

    editplaylist.handlebars created.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const spotifyRoutes = require('./spotifyRoutes');
 const playlistRoutes = require('./playlistRoutes');
+const songRoutes = require('./songRoutes');
 
 router.use('/users', userRoutes);
 router.use('/spotify', spotifyRoutes);
 router.use('/playlist', playlistRoutes);
+router.use('/song', songRoutes);
 
 
 module.exports = router;
---
+++ controllers/api/songRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { Playlist, Song } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+
+module.exports = router;
+
+
